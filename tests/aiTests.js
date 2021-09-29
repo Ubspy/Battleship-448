d60fce75d47c9c68e7800b05437a2fe6b3345919
Ubspy <jack-m@live.com>
2021-09-29 11:21:08 -0500

    Changed aiTests file to just have test functions that can be ran in the console

--- tests/aiTests.js
+++ tests/aiTests.js
@@ -13,13 +13,13 @@ function testEasyAI() {
 // on the ship, and we add one because there's a chance that you hit the middle of the ship, so the AI will go until it misses, the go the opposite
 // direction from the original hit point
 function testMediumAI() {
-    let aiBoard = new board();
+    let aiBoard = new board(6);
     
     // TODO: This needs to be implemented
     let mediumAI = new AI("medium");
 
     // TODO: This needs to be implemented
-    let ships = mediumAI.placeShips(aiBoard, 6);
+    let ships = mediumAI.placeShips(aiBoard);
 
     let currentHit = null;
 
@@ -81,7 +81,7 @@ function easyFullTest() {
     // Make a new board for the player and the AI 
     aiBoard = new board();
 
-    // // TODO: This function needs to be added
+    / TODO: This function needs to be added
     // Makes a new AI object
     easyAI = new AI("easy");
 
@@ -146,6 +146,7 @@ function validPlacementTest(aiObject) {
     for (var i = 0; i < 10; i++) {
         let board = new board();
         aiObject.placeShips(board, 6);
+
         let shipSquares = board.board.reduce((prevSum, currArr) => prevSum + currArr.reduce((prevSum2, currSquare) => {
             if (currSquare instanceof ship) {
                 return prevSum2 + 1;
@@ -167,12 +168,13 @@ function randomPlacementTest(difficulty) {
 
     let aiOne = new AI(difficulty);
     let aiTwo = new AI(difficulty);
-    let boardOne = new board();
-    let boardTwo = new board();
+
+    let boardOne = new board(6);
+    let boardTwo = new board(6);
 
     aiOne.placeShips(boardOne, 6);
     aiTwo.placeShips(boardTwo, 6);
-
+    
     return randomBoardTest(boardOne, boardTwo); // not sure what arithmetic to do from here but this is a start.
 }
 
@@ -180,11 +182,15 @@ function testHardAI() {
     // Creates a hard AI object and a board. Places 6 ships on the board.
     // Has the AI fire 6+5+4+3+2+1 = 21 times. Since it cheats, it should have hit every time and not missed a single shot.
     let ai = new AI("hard");
-    let board = new board();
+
+    let board = new board(6);
+
     ai.placeShips(board, 6);
+
     for (let i = 0; i < 21; i++) {
         ai.fire();
     }
+
     return board.allSunk();
 }
 
